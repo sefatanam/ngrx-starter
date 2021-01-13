8d22658376584a84941fae0c360a4f4a424be6bc
sefatanam <sefatanam@gmail.com>
2021-01-14 00:14:55 +0600

    basic setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngrx-starter';
+export class AppComponent implements OnInit {
+
+  count: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count = store.select('count');
+  }
+
+  ngOnInit(): void {
+  }
+
 }
