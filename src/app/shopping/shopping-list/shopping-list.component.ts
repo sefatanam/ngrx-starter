52953c363b1b6ebcf566c8effcc061d5ec0d355d
sefatanam <sefatanam@gmail.com>
2021-01-16 15:24:06 +0600

    move to a module

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -3,7 +3,7 @@ import {Observable} from 'rxjs';
 import {ShoppingItem} from '../../store/models/interfaces/ShoppingItem';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../store/states/AppState';
-import {AddItemAction, DeleteItemAction} from '../../store/actions/shopping.action';
+import {AddItemAction, DeleteItemAction, LoadShoppingAction} from '../../store/actions/shopping.action';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,27 +11,25 @@ import {AddItemAction, DeleteItemAction} from '../../store/actions/shopping.acti
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
-
   shoppingItems$: Observable<Array<ShoppingItem>>;
   loading$: Observable<Boolean>;
   error$: Observable<number | Error>;
-  newShoppingItem: ShoppingItem = {id:0, name: ''};
+  newShoppingItem: ShoppingItem = {id: 0, name: ''};
 
   constructor(private stores: Store<AppState>) {
   }
 
   ngOnInit(): void {
-    // this.shoppingItem$ = this.store.select(store => store.shopping);
     this.shoppingItems$ = this.stores.select(store => store.shopping.list);
     this.loading$ = this.stores.select(store => store.shopping.loading);
     this.error$ = this.stores.select(store => store.shopping.error);
 
+    this.stores.dispatch(new LoadShoppingAction());
   }
 
   addItem($event): void {
     $event.preventDefault();
-    this.newShoppingItem.id =  Math.random() * 0.23;
+    this.newShoppingItem.id = Math.random() * 0.23;
     this.stores.dispatch(new AddItemAction(this.newShoppingItem));
     this.newShoppingItem = {id: 0, name: ''};
   }
@@ -39,5 +37,6 @@ export class ShoppingListComponent implements OnInit {
   deleteItem(id: number): void {
     this.stores.dispatch(new DeleteItemAction(id));
   }
+
 
 }
